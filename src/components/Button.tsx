9c2481be37a2765b582cd98f931c1694cd1c0a46
Oleg Shilov <me@olegshilov.com>
2022-08-01 20:04:47 +0300

    fix(ui-kit): add color transitions and remove appearance from `Button`

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -27,8 +27,8 @@ export function Button({
       ? 'bg-transparent text-primary border-[2px] box-border border-solid border-primary hover:text-[#20d775] hover:border-[#20d775]'
       : 'bg-primary text-white hover:bg-[#20d775]',
     'text-base font-semibold font-sans leading-[24px]',
-    'rounded-[8px] py-[8px] px-[16px]',
-    'transition-colors duration-[125ms]',
+    'rounded-[8px] py-[8px] px-[22px] appearance-none',
+    'transition-colors duration-150 ease-linear',
     fill ? 'w-full' : 'inline-block',
     { 'opacity-75 not-allowed': disabled },
     className,

0b6c4095a5a65a638e3a7a49a9cbcda798d7bccd
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-08-01 22:22:01 +0300

    fix(ui-kit): used Typography components in AddNetwork

--- src/components/AddNetwork.tsx
+++ src/components/AddNetwork.tsx
@@ -1,43 +1,37 @@
 import React, { ReactElement } from 'react';
 import { Button } from './Button';
+import { Heading, Text } from './Typography';
 
 export function AddNetwork(): ReactElement {
-  const boldSpanClassNames =
-    'font-[700] font-size-[16px] leading-[24px] text-black mb-[4px]';
-  const lightSpanClassNames =
-    'font-[400] font-size-[16px] leading-[24px] text-dark-gray';
   const smallContainerClassNames = 'flex flex-col mb-[12px]';
 
   return (
     <div className="bg-transparent">
-      <div className="font-serif font-[700] font-size-[38px] leading-[48px] text-black mb-[20px] ">
-        Allow to add a network
-      </div>
+      <Heading level={2}>Allow to add a network</Heading>
       <div className="font-sans mb-[32px]">
-        <span className={lightSpanClassNames}>
+        <Text color="light">
           This will allow this network to be used within MetaMask.
-        </span>
+        </Text>
         <div className="bg-white rounded-[12px] p-[20px] mt-[24px]">
           <div className={smallContainerClassNames}>
-            <span className={boldSpanClassNames}> Network Name: </span>
-            <span className={lightSpanClassNames}> Haqq Network Testnet </span>
+            <Text bold> Network Name: </Text>
+            <Text color="light"> Haqq Network Testnet </Text>
           </div>
           <div className={smallContainerClassNames}>
-            <span className={boldSpanClassNames}> Network Url: </span>
-            <span className={lightSpanClassNames}>
-              https://rpc.eth.testedge.haqq.network
-            </span>
+            <Text bold> Network Url: </Text>
+            <Text color="light">https://rpc.eth.testedge.haqq.network</Text>
           </div>
           <div className={smallContainerClassNames}>
-            <span className={boldSpanClassNames}> Chain ID: </span>
-            <span className={lightSpanClassNames}> 53211 </span>
+            <Text bold> Chain ID: </Text>
+            <Text color="light"> 53211 </Text>
           </div>
-          <span className="text-primary font-bold"> View all details </span>
+          <Text bold color="primary">
+            View all details
+          </Text>
         </div>
       </div>
       <div className="flex flex-row-reverse">
-        {/* How to correctly pass `Children` properties in this case?  */}
-        <Button children={'Approve'} type={'submit'} />
+        <Button type={'submit'}>Approve</Button>
       </div>
     </div>
   );

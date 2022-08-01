171ba4cebc2c159fe16928b708a3e43638f6f547
Oleg Shilov <me@olegshilov.com>
2022-08-01 20:05:59 +0300

    fix(ui-kit): udate styles and add controlled shadow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,29 @@
+import styled from '@emotion/styled';
+import clsx from 'clsx';
 import React, { ReactElement, ReactNode } from 'react';
 
-export function Card({ children }: { children: ReactNode }): ReactElement {
-  return <div className="bg-white text-black rounded-[32px]">{children}</div>;
+interface CardProps {
+  children: ReactNode;
+  className?: string;
+  withShadow?: boolean;
+}
+
+const CardStyled = styled.div<{ withShadow?: boolean }>`
+  box-shadow: ${({ withShadow }) => {
+    return withShadow ? '0px 8px 24px rgba(15, 30, 51, 0.08)' : 'none';
+  }};
+`;
+
+export function Card({
+  children,
+  className,
+  withShadow = false,
+}: CardProps): ReactElement {
+  const classNames = clsx('bg-white rounded-[12px]', className);
+
+  return (
+    <CardStyled className={classNames} withShadow={withShadow}>
+      {children}
+    </CardStyled>
+  );
 }

1e70d0781edf75227ae5b91db899a3f169243345
Oleg Shilov <me@olegshilov.com>
2022-08-04 19:56:18 +0300

    fix: lint, cleanup and format

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,9 +9,9 @@ interface CardProps {
 }
 
 const CardStyled = styled.div<{ withShadow?: boolean }>`
-  box-shadow: ${({ withShadow }) => {
+  /* box-shadow: ${({ withShadow }) => {
     return withShadow ? '0px 8px 24px rgba(15, 30, 51, 0.08)' : 'none';
-  }};
+  }}; */
 `;
 
 export function Card({
@@ -19,7 +19,7 @@ export function Card({
   className,
   withShadow = false,
 }: CardProps): ReactElement {
-  const classNames = clsx('bg-white rounded-[12px]', className);
+  const classNames = clsx('bg-white rounded-[12px] shadow-sm', className);
 
   return (
     <CardStyled className={classNames} withShadow={withShadow}>

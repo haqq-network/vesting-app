0f483a1b5c9b50e6d889dda7e11671a0feac0a1c
Oleg Shilov <me@olegshilov.com>
2022-08-04 19:38:49 +0300

    feat: implements connect button and account widget

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,5 +1,44 @@
+import React, { useMemo } from 'react';
 import { ReactElement } from 'react';
+import { Chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+// import { InjectedConnector } from 'wagmi/connectors/injected';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { getChain } from './config';
+
+function WagmiContainer({ children }: { children: ReactElement }) {
+  const chain = getChain();
+
+  const { provider: rpcProvider, webSocketProvider } = useMemo(() => {
+    return configureChains(
+      [chain],
+      [
+        jsonRpcProvider({
+          rpc: (chain: Chain) => {
+            return {
+              http: chain.rpcUrls.default,
+              webSocket: chain.rpcUrls.ws,
+            };
+          },
+        }),
+      ],
+    );
+  }, [chain]);
+  const client = useMemo(() => {
+    return createClient({
+      provider: rpcProvider,
+      webSocketProvider,
+      connectors: [
+        // new InjectedConnector({ chains: [chain] }),
+        new MetaMaskConnector({ chains: [chain] }),
+      ],
+      // autoConnect: true,
+    });
+  }, [chain, rpcProvider, webSocketProvider]);
+
+  return <WagmiConfig client={client}>{children}</WagmiConfig>;
+}
 
 export function AppContainer({ children }: { children: ReactElement }) {
-  return children;
+  return <WagmiContainer>{children}</WagmiContainer>;
 }

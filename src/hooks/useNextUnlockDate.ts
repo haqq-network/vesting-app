bacbdc8e05dfdcc38a4ec7cc8cf2474740bec284
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-08-08 02:23:42 +0300

    fix(utils): remove hook parameter and change test

--- src/hooks/useNextUnlockDate.ts
+++ src/hooks/useNextUnlockDate.ts
@@ -13,8 +13,8 @@ interface NextUnlockDateHook {
 export function useNextUnlockDate(
   fromDate: Date,
   params: { daysBetween: number },
-  now: Date = new Date(),
 ): NextUnlockDateHook {
+  const now = new Date();
   let currentDate = fromDate.getTime();
   const offset = params.daysBetween * 24 * 60 * 60 * 1000;
   while (currentDate <= now.valueOf()) currentDate += offset;

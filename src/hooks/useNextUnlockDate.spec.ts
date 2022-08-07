8defd869029f8566656bfdb479ce7b06278a7793
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-08-08 02:23:42 +0300

    fix(utils): remove hook parameter and change test

--- src/hooks/useNextUnlockDate.spec.ts
+++ src/hooks/useNextUnlockDate.spec.ts
@@ -1,34 +1,23 @@
 import { renderHook } from '@testing-library/react';
 import { useNextUnlockDate } from './useNextUnlockDate';
 
-describe('useNextUnlockDate()', () => {
-  it('should return correct next date with period === 20', () => {
+jest.useFakeTimers().setSystemTime(new Date('2022-05-01'));
+
+const periods = [
+  [20, '2022-05-21T00:00:00.000Z'],
+  [50, '2022-05-31T00:00:00.000Z'],
+];
+
+test.each(periods)(
+  'should return correct next date with period',
+  (period, expectedReturnDate) => {
     const startDate = new Date('2022-01-01T00:00:00.000Z');
-    const period = 20;
-    const nowDate = new Date('2022-05-01T00:00:00.000Z');
-    const expectedReturnDate = '2022-05-21T00:00:00.000Z';
     const { result } = renderHook(() =>
-      useNextUnlockDate(startDate, { daysBetween: period }, nowDate),
+      useNextUnlockDate(startDate, { daysBetween: +period }),
     );
 
     expect(result.current.nextUnlockDate.toISOString()).toEqual(
       expectedReturnDate,
     );
-  });
-});
-
-describe('useNextUnlockDate()', () => {
-  it('should return correct next date with period === 50', () => {
-    const startDate = new Date('2022-01-01T00:00:00.000Z');
-    const period = 50;
-    const nowDate = new Date('2022-05-01T00:00:00.000Z');
-    const expectedReturnDate = '2022-05-31T00:00:00.000Z';
-    const { result } = renderHook(() =>
-      useNextUnlockDate(startDate, { daysBetween: period }, nowDate),
-    );
-
-    expect(result.current.nextUnlockDate.toISOString()).toEqual(
-      expectedReturnDate,
-    );
-  });
-});
+  },
+);

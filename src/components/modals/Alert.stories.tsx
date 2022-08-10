2b08bec48df354f2e031fa123612701a8438e97c
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-08-10 16:26:00 +0300

    fix(ui-kit): clean alert component, add userstory

--- src/components/modals/Alert.stories.tsx
+++ src/components/modals/Alert.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Alert as AlertComponent, AlertProps } from './Alert';
+import { Confirm as ConfirmComponent, ConfirmProps } from './Alert';
 
 export default {
   title: 'ui-kit/Modals',
@@ -14,10 +15,23 @@ const Template: ComponentStory<typeof AlertComponent> = (args: AlertProps) => (
   <AlertComponent {...args} />
 );
 
+const ConfirmTemplate: ComponentStory<typeof ConfirmComponent> = (
+  args: ConfirmProps,
+) => <ConfirmComponent {...args} />;
+
 export const Alert = Template.bind({});
 Alert.args = {
   isOpen: true,
   title: 'Example alert',
   children:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum perspiciatis pariatur praesentium possimus exercitationem blanditiis atque laborum, fuga dolorum ab odit aut minima, dolor doloribus delectus voluptas minus aspernatur cumque?',
+};
+
+export const Confirm = ConfirmTemplate.bind({});
+Confirm.args = {
+  isOpen: true,
+  title: 'Example alert',
+  children:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum perspiciatis pariatur praesentium possimus exercitationem blanditiis atque laborum, fuga dolorum ab odit aut minima, dolor doloribus delectus voluptas minus aspernatur cumque?',
+  buttonTitle: 'Confirm',
 };

646f57ca11a2a95e1a0f948e20565f0acf37e210
Oleg Shilov <me@olegshilov.com>
2022-08-10 07:19:48 +0300

    fix(hooks): simplify `useNextUnlockDate` hook, refactor tests

--- src/hooks/useNextUnlockDate.spec.ts
+++ src/hooks/useNextUnlockDate.spec.ts
@@ -1,23 +1,30 @@
 import { renderHook } from '@testing-library/react';
 import { useNextUnlockDate } from './useNextUnlockDate';
 
-jest.useFakeTimers().setSystemTime(new Date('2022-05-01'));
-
-const periods = [
-  [20, '2022-05-21T00:00:00.000Z'],
-  [50, '2022-05-31T00:00:00.000Z'],
+const periods: [number, Date][] = [
+  [20, new Date('2022-05-21T00:00:00.000Z')],
+  [50, new Date('2022-05-31T00:00:00.000Z')],
 ];
 
-test.each(periods)(
-  'should return correct next date with period',
-  (period, expectedReturnDate) => {
-    const startDate = new Date('2022-01-01T00:00:00.000Z');
-    const { result } = renderHook(() =>
-      useNextUnlockDate(startDate, { daysBetween: +period }),
-    );
+describe('useNextUnlockDate()', () => {
+  beforeEach(() => {
+    jest.useFakeTimers().setSystemTime(new Date('2022-05-01T00:00:00.000Z'));
+  });
 
-    expect(result.current.nextUnlockDate.toISOString()).toEqual(
-      expectedReturnDate,
-    );
-  },
-);
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test.each(periods)(
+    'should return correct next date with period',
+    (period, expectedDate) => {
+      const startDate = new Date('2022-01-01T00:00:00.000Z');
+      const { result } = renderHook(() =>
+        useNextUnlockDate(startDate, { period }),
+      );
+
+      expect(result.current.toISOString()).toEqual(expectedDate.toISOString());
+      expect(result.current.getTime()).toEqual(expectedDate.getTime());
+    },
+  );
+});
--- src/hooks/useNextUnlockDate.ts
+++ src/hooks/useNextUnlockDate.ts
@@ -1,27 +1,20 @@
 import { useMemo } from 'react';
 
-interface NextUnlockDateHook {
-  nextUnlockDate: Date;
-  // time: {
-  //   d: number;
-  //   h: number;
-  //   m: number;
-  //   s: number;
-  // };
-}
-
 export function useNextUnlockDate(
-  fromDate: Date,
-  params: { daysBetween: number },
-): NextUnlockDateHook {
-  return useMemo(() => {
+  startDate: Date,
+  { period }: { period: number },
+): Date {
+  const nextUnlockDate = useMemo(() => {
     const now = Date.now();
-    let unlockTime = fromDate.getTime();
-    const offset = params.daysBetween * 24 * 60 * 60 * 1000;
+    const offset = period * 24 * 60 * 60 * 1000;
+    let unlockTime = startDate.getTime();
+
     while (unlockTime <= now) {
       unlockTime += offset;
     }
 
-    return { nextUnlockDate: new Date(unlockTime) };
-  }, [fromDate, params.daysBetween]);
+    return new Date(unlockTime);
+  }, [startDate, period]);
+
+  return nextUnlockDate;
 }

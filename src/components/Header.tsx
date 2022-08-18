32f9192eb14ac5622cc11db16e17e4b7f2a9913f
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-08-18 18:22:34 +0300

    feat(ui-kit): add burger menu button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React, { lazy, ReactElement, useMemo } from 'react';
+import React, { lazy, ReactElement, useMemo, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { BurgerButton } from './BurgerButton';
 import { HeaderLink } from './HeaderLink';
 import { Container } from './Layout';
 import { Logo } from './Logo';
@@ -12,6 +13,7 @@ const ConnectButton = lazy(() => import('./ConnectButton'));
 const NetworkStatus = lazy(() => import('./NetworkStatus'));
 
 export function Header({ links }: HeaderProps): ReactElement {
+  const [isOpen, setOpen] = useState<boolean>(false);
   const headerLinks = useMemo(() => {
     if (!links || links.length === 0) {
       return null;
@@ -29,48 +31,12 @@ export function Header({ links }: HeaderProps): ReactElement {
       <Container>
         <div className="relative flex items-center justify-between h-[64px]">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                aria-hidden="true"
-                // onClick={handleBurgerMenuClick}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                aria-hidden="true"
-                // onClick={handleBurgerMenuClick}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+            <BurgerButton
+              onClick={() => {
+                setOpen(!isOpen);
+              }}
+              isOpen={isOpen}
+            />
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
